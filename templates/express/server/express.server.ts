c419e2c8ad5f5b67347b71cd6c4c9894dbe5866d
cplepage <lepage.charlesphilippe@gmail.com>
2022-12-02 09:25:39 -0500

    express

--- templates/express/server/express.server.ts
+++ templates/express/server/express.server.ts
@@ -2,10 +2,14 @@ import Server from "fullstacked/server";
 import express from "express"
 
 const app = express();
-app.use(express.static(Server.publicDir));
 
 app.get("/hello-world", (req, res) => {
     res.send("Hello World");
 });
 
-Server.addListener(app);
+const { promisifiedListener, resolver } = Server.promisify(app);
+
+// express bottoms out here
+app.use(resolver)
+
+Server.addListener(promisifiedListener)
